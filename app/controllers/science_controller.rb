f685d5f0edf4110bc2cdbc938524e0dfed95bc62
Christian Alic Kelley <cakelley@colorado.edu>
2015-04-23 08:59:32 +0000

    Heroku update.

--- app/controllers/science_controller.rb
+++ app/controllers/science_controller.rb
@@ -1,4 +1,24 @@
 class ScienceController < ApplicationController
     def shome
     end
+    def generalphysics
+    end
+    def astronomy
+    end
+    def electromagnetism
+    end
+    def mechanicsdynamicslagrangianhamiltonian
+    end
+    def quantummechanics
+    end
+    def thermodynamicsstatisticalmechanics
+    end
+    def nuclearparticlephysics
+    end
+    def astrophysics
+    end
+    def relativity
+    end
+    def fluiddynamics
+    end
 end
